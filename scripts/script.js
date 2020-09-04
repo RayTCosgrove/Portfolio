da796b4b0ab0b1dd8438dd309b51d92066a28938
raytcosgrove <raytcosgrove@gmail.com>
2020-09-04 13:45:35 -0400

    about section stuff

--- scripts/script.js
+++ scripts/script.js
@@ -26,9 +26,9 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-document.getElementById("socials-left").style.left = "40px";
-document.getElementById("socials-right").style.right = "40px";
-document.getElementById("raymie").style.right = "25%";
+document.getElementById("socials-left").style.left = "30px";
+document.getElementById("socials-right").style.right = "30px";
+document.getElementById("raymie").style.right = "20%";
 
   
 
