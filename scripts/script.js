e358a7cd840607eb4814970d20c7aff5668e5f6c
Raymond Cosgrove <raytcosgrove@gmail.com>
2020-09-11 16:55:23 -0400

    all webp with jpg backups

--- scripts/script.js
+++ scripts/script.js
@@ -52,3 +52,13 @@ document.getElementById("raymie").style.right = "20%";
 
 
 };
+
+window.onresize = function() {
+
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  $("#topwave").css("top", innerHeight);
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+}
