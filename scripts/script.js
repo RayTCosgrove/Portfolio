3d23ca3aa205733e97f310f89ba641c772f4735f
Raymond Cosgrove <raytcosgrove@gmail.com>
2020-09-13 16:35:19 -0400

    fixed viewport resizing issue on mobile

--- scripts/script.js
+++ scripts/script.js
@@ -56,9 +56,11 @@ document.getElementById("raymie").style.right = "20%";
 window.onresize = function() {
 
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  $("#topwave").css("top", innerHeight);
+
+ if(window.innerWidth>450) {
+ $("#topwave").css("top", innerHeight);
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-
+ }
 }
