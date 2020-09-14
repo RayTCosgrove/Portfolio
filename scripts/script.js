ac2e691d201c19a51c5dd948da07cb8ae7301edb
Raymond Cosgrove <raytcosgrove@gmail.com>
2020-09-14 12:32:06 -0400

    innerWidth change

--- scripts/script.js
+++ scripts/script.js
@@ -21,7 +21,7 @@ console.log(currentScrollPos)
 
 window.onload = function () {
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  $("#topwave").css("top", innerHeight);
+  $("#topwave").css("top", window.innerHeight);
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -58,7 +58,7 @@ window.onresize = function() {
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 
  if(window.innerWidth>515) {
- $("#topwave").css("top", innerHeight);
+ $("#topwave").css("top", window.innerHeight);
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
